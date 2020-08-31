6866bbc981c42b52123d5ddf75b22d162b1bfc4e
Falon Landers <falonlanders@outlook.com>
2020-08-31 11:51:23 -0400

    whatevs

---
+++ build/precache-manifest.fd4390a20c149fd1dc6ffd6d73f01dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9215a004ce23493e2c6d14c9327b90e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5438a94cf8f856516aa0",
+    "url": "/static/css/main.de1488c4.chunk.css"
+  },
+  {
+    "revision": "c3958231d0c710a39cd6",
+    "url": "/static/js/2.a250e9be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a250e9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5438a94cf8f856516aa0",
+    "url": "/static/js/main.e4606487.chunk.js"
+  },
+  {
+    "revision": "8ad5011af74688783bec",
+    "url": "/static/js/runtime-main.5894a212.js"
+  }
+]);
